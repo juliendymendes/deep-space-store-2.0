fad49cce198de21f8e708440f487ecdba5af6e16
Juliendy Mendes <juliendy0708@gmail.com>
2024-06-21 23:51:45 -0400

    feat: form validation and input masks

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,78 +23,128 @@
 			</v-col>
 
 			<v-col cols="12" md="6">
-				<v-stepper
-					alt-labels
-					editable
-					color="primary"
-					prev-text="Anterior"
-					next-text="Próximo"
-					:items="['Dados pessoais', 'Entrega', 'Pagamento']">
-					<template v-slot:item.1>
-						<v-form fast-fail>
-							<v-text-field v-model="personalData.name" label="Nome completo"></v-text-field>
+				<v-stepper alt-labels editable color="primary" v-model="stepper">
+					<v-stepper-header>
+						<v-stepper-item title="Dados pessoais" value="1" complete editable> </v-stepper-item>
 
-							<v-text-field v-model="personalData.email" label="Email"></v-text-field>
-							<v-text-field v-model="personalData.phone" label="Telefone"></v-text-field>
-						</v-form>
-					</template>
+						<v-stepper-item title="Entrega" value="2" complete editable> </v-stepper-item>
 
-					<template v-slot:item.2>
-						<v-form fast-fail>
-							<v-text-field v-model="deliveryData.cep" label="CEP"></v-text-field>
+						<v-stepper-item title="Pagamento" value="3" complete editable> </v-stepper-item>
+					</v-stepper-header>
 
-							<v-row>
-								<v-col cols="12" sm="8">
-									<v-text-field v-model="deliveryData.logradouro" label="Rua"></v-text-field>
-								</v-col>
-								<v-col cols="12" sm="4">
-									<v-text-field v-model="deliveryData.numero" label="Número"></v-text-field>
-								</v-col>
-							</v-row>
-							<v-text-field v-model="deliveryData.bairro" label="Bairro"></v-text-field>
-							<v-row>
-								<v-col cols="12" sm="8">
-									<v-text-field v-model="deliveryData.localidade" label="Cidade"></v-text-field>
-								</v-col>
-								<v-col cols="12" sm="4">
-									<v-text-field v-model="deliveryData.uf" label="UF"></v-text-field>
-								</v-col>
-							</v-row>
-							<v-text-field v-model="deliveryData.complemento" label="Complemento"></v-text-field>
-						</v-form>
-					</template>
+					<v-stepper-window v-model="stepper">
+						<v-stepper-window-item value="1">
+							<v-form ref="personalDataForm">
+								<v-text-field
+									v-model="personalData.name"
+									:rules="[rules.required]"
+									label="Nome completo"></v-text-field>
 
-					<template v-slot:item.3>
-						<v-radio-group v-model="paymentData.paymentType" inline>
-							<v-radio color="primary" label="PIX" value="pix"></v-radio>
-							<v-radio color="primary" label="Cartão de crédito" value="credito"></v-radio>
-							<v-radio color="primary" label="Boleto" value="boleto"></v-radio>
-						</v-radio-group>
+								<v-text-field
+									type="email"
+									v-model="personalData.email"
+									:rules="[rules.email]"
+									label="Email"></v-text-field>
+								<v-text-field
+									v-maska="'(##) ####-####'"
+									v-model="personalData.phone"
+									:rules="[rules.required]"
+									label="Telefone"></v-text-field>
+							</v-form>
+						</v-stepper-window-item>
 
-						<v-form fast-fail>
-							<v-text-field v-model="paymentData.cpf" label="CPF"></v-text-field>
-
-							<div v-if="paymentData.paymentType === 'credito'">
-								<v-text-field v-model="paymentData.cardNumber" label="Número do cartão"></v-text-field>
-
-								<v-text-field v-model="paymentData.cardOwnerName" label="Nome no cartão"></v-text-field>
+						<v-stepper-window-item value="2">
+							<v-form ref="deliveryDataForm">
+								<v-text-field
+									v-maska="'#####-###'"
+									v-model="deliveryData.cep"
+									:rules="[rules.required]"
+									label="CEP"></v-text-field>
 
 								<v-row>
 									<v-col cols="12" sm="8">
-										<v-text-field v-model="paymentData.cardExpirationDate" label="Data de validade"></v-text-field>
+										<v-text-field
+											v-model="deliveryData.logradouro"
+											:rules="[rules.required]"
+											label="Rua"></v-text-field>
 									</v-col>
 									<v-col cols="12" sm="4">
-										<v-text-field v-model="paymentData.cardSecurityCode" label="CVV"></v-text-field>
+										<v-text-field v-model="deliveryData.numero" :rules="[rules.required]" label="Número"></v-text-field>
 									</v-col>
 								</v-row>
-							</div>
-							<v-row>
-								<v-col cols="12" class="d-flex justify-end">
-									<v-btn color="primary" @click="finalizeOrder">Finalizar pedido</v-btn>
-								</v-col>
-							</v-row>
-						</v-form>
-					</template>
+								<v-text-field v-model="deliveryData.bairro" :rules="[rules.required]" label="Bairro"></v-text-field>
+								<v-row>
+									<v-col cols="12" sm="8">
+										<v-text-field
+											v-model="deliveryData.localidade"
+											:rules="[rules.required]"
+											label="Cidade"></v-text-field>
+									</v-col>
+									<v-col cols="12" sm="4">
+										<v-text-field
+											v-model="deliveryData.uf"
+											v-maska="'@@'"
+											:rules="[rules.required]"
+											label="UF"></v-text-field>
+									</v-col>
+								</v-row>
+								<v-text-field v-model="deliveryData.complemento" label="Complemento"></v-text-field>
+							</v-form>
+						</v-stepper-window-item>
+
+						<v-stepper-window-item value="3">
+							<v-radio-group v-model="paymentData.paymentType" inline>
+								<v-radio color="primary" label="PIX" value="pix"></v-radio>
+								<v-radio color="primary" label="Cartão de crédito" value="credito"></v-radio>
+								<v-radio color="primary" label="Boleto" value="boleto"></v-radio>
+							</v-radio-group>
+
+							<v-form ref="paymentDataForm">
+								<v-text-field
+									v-model="paymentData.cpf"
+									v-maska="'###.###.###-##'"
+									:rules="[rules.required]"
+									label="CPF"></v-text-field>
+
+								<div v-if="paymentData.paymentType === 'credito'">
+									<v-text-field
+										v-maska="'####.####.####.####'"
+										v-model="paymentData.cardNumber"
+										:rules="[rules.required]"
+										label="Número do cartão"></v-text-field>
+
+									<v-text-field
+										v-model="paymentData.cardOwnerName"
+										:rules="[rules.required]"
+										label="Nome no cartão"></v-text-field>
+
+									<v-row>
+										<v-col cols="12" sm="8">
+											<v-text-field
+												v-model="paymentData.cardExpirationDate"
+												v-maska="'##/####'"
+												:rules="[rules.required]"
+												label="Data de validade"></v-text-field>
+										</v-col>
+										<v-col cols="12" sm="4">
+											<v-text-field
+												type="number"
+												v-model="paymentData.cardSecurityCode"
+												:rules="[rules.required]"
+												label="CVV"></v-text-field>
+										</v-col>
+									</v-row>
+								</div>
+								<v-row>
+									<v-col cols="12" class="d-flex justify-end">
+										<v-btn color="primary" @click="finalizeOrder">Finalizar pedido</v-btn>
+									</v-col>
+								</v-row>
+							</v-form>
+						</v-stepper-window-item>
+					</v-stepper-window>
+					<v-stepper-actions next-text="Próximo" prev-text="Anterior" @click:next="next" @click:prev="stepper--">
+					</v-stepper-actions>
 				</v-stepper>
 			</v-col>
 		</v-row>
@@ -106,6 +156,8 @@ import DeliveryAddress from '@/types/DeliveryAddress';
 import PaymentData from '@/types/PaymentData';
 import PersonalData from '@/types/PersonalData';
 import { ref } from 'vue';
+import { vMaska } from 'maska/vue';
+const stepper = ref(0);
 
 const personalData = ref<PersonalData>({
 	name: '',
@@ -128,15 +180,69 @@ const paymentData = ref<PaymentData>({
 	cpf: '',
 });
 
+const rules = ref({
+	required: (value: string) => {
+		if (value) return true;
+		return 'Campo obrigatório.';
+	},
+	email: (value: string) => {
+		if (value) {
+			const pattern =
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return pattern.test(value) || 'E-mail inválido';
+		}
+	},
+});
+
+const personalDataForm = ref<HTMLFormElement>();
+const deliveryDataForm = ref<HTMLFormElement>();
+const paymentDataForm = ref<HTMLFormElement>();
+
 const colors = ref(['indigo', 'warning', 'pink darken-2', 'red lighten-1', 'deep-purple accent-4']);
 const slides = ref(['First', 'Second', 'Third', 'Fourth', 'Fifth']);
 
-function finalizeOrder() {
-	console.log('Personal data');
-	console.log(personalData.value);
-	console.log('Delivery data');
-	console.log(deliveryData.value);
-	console.log('Payment data');
-	console.log(paymentData.value);
+async function finalizeOrder() {
+	const valid = await validateForms();
+	if (valid) {
+		console.log('Personal data');
+		console.log(personalData.value);
+		console.log('Delivery data');
+		console.log(deliveryData.value);
+		console.log('Payment data');
+		console.log(paymentData.value);
+	} else {
+		console.log('dados inválidos');
+	}
+}
+
+// Valida o formulário antes de passar para o próximo
+async function next() {
+	switch (stepper.value) {
+		case 0: {
+			const result = await personalDataForm.value?.validate();
+			result.valid ? stepper.value++ : console.log('Personal data não válido');
+			break;
+		}
+
+		case 1: {
+			const result = await deliveryDataForm.value?.validate();
+			result.valid ? stepper.value++ : console.log('Delivery data não válido');
+			break;
+		}
+
+		case 2: {
+			const result = await paymentDataForm.value?.validate();
+			result.valid ? stepper.value++ : console.log('payment data não válido');
+			break;
+		}
+	}
+}
+
+async function validateForms() {
+	const personalDataResult = await personalDataForm.value?.validate();
+	const deliveryDataResult = await deliveryDataForm.value?.validate();
+	const paymentDataResult = await paymentDataForm.value?.validate();
+	if (personalDataResult.valid && deliveryDataResult.valid && paymentDataResult.valid) return true;
+	return false;
 }
 </script>
